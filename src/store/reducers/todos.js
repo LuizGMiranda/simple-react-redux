d68050052dacaa2ee505200121a5aa629bfbbc55
Luiz Gustavo Miranda <luiz.g.miranda@gmail.com>
2019-05-28 00:08:46 -0300

    Subindo arquivos

---
+++ src/store/reducers/todos.js
@@ -0,0 +1,16 @@
+const INITIAL_STATE = [
+  { id: 1, text: "ReactJS" },
+  { id: 2, text: "ReactNative" },
+  { id: 3, text: "Redux" }
+];
+
+export default function todos(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, { id: Math.random(), text: action.payload.text }];
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.payload.id);
+    default:
+      return state;
+  }
+}
